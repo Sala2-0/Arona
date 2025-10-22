0140e871a78d3fad8f1812a58620015c9e1c12d5
Sala2-0 <id.gnu.cc@gmail.com>
2025-10-22 21:23:36 +0200

    Subcommand: activity
    Shows a player's most active seasons

--- Arona/Commands/ClanBattleStats.cs
+++ Arona/Commands/ClanBattleStats.cs
@@ -9,10 +9,11 @@ using Arona.Utility;
 
 namespace Arona.Commands;
 
+[SlashCommand("cb_stats", "Clan battle stats of a player")]
 public class ClanBattleStats : ApplicationCommandModule<ApplicationCommandContext>
 {
-    [SlashCommand("cb_stats", "Clan battle stats of a player")]
-    public async Task BaseExpAddAsync(
+    [SubSlashCommand("season", "One season")]
+    public async Task SeasonAsync(
         [SlashCommandParameter(Name = "player", Description = "Player in question", AutocompleteProviderType = typeof(PlayerAutocomplete))]
         string accountData,
 
@@ -81,6 +82,73 @@ public class ClanBattleStats : ApplicationCommandModule<ApplicationCommandContex
                     $"**D/B:** {Math.Round((double)playerSeasonData.DamageDealt / playerSeasonData.Battles).ToString(System.Globalization.CultureInfo.InvariantCulture)}\n" +
                     $"**K/B:** {Math.Round((double)playerSeasonData.Kills / playerSeasonData.Battles, 2).ToString(System.Globalization.CultureInfo.InvariantCulture)}",
             });
+        }
+        catch (Exception ex)
+        {
+            await Program.Error(ex);
+            await deferredMessage.EditAsync("API error >_<");
+        }
+    }
+
+    [SubSlashCommand("activity", "List showing a player's most active seasons")]
+    public async Task ActivityAsync(
+        [SlashCommandParameter(Name = "player", Description = "Player in question", AutocompleteProviderType = typeof(PlayerAutocomplete))]
+        string accountData
+    )
+    {
+        var deferredMessage = new DeferredMessage { Interaction = Context.Interaction };
+        await deferredMessage.SendAsync();
+
+        Guild.Exists(Context.Interaction);
+
+        var self = await Program.Client!.Rest.GetCurrentUserAsync();
+        var botIconUrl = self.GetAvatarUrl()!.ToString();
+
+        var split = accountData.Split(',');
+        var accountId = split[0];
+        var rawName = split[1];
+        var region = split[2];
+
+        var name = "";
+
+        foreach (var letter in rawName)
+            if (letter == '_')
+                name += "\\_";
+            else
+                name += letter;
+
+        try
+        {
+            var data = await ClanBattleSeasonStats.GetAsync(accountId, region);
+            var seasonData = await ClanBattleSeasons.GetAsync();
+
+            var filtered = data[accountId].Seasons
+                .Where(c => c.Battles > 0)
+                .ToList();
+            filtered.Sort((a, b) => b.Battles - a.Battles);
+
+            var processed = filtered.Select(c => new
+            {
+                Id = c.SeasonId,
+                Name = seasonData.First(s => s.Value.SeasonId == c.SeasonId).Value.Name,
+                BattlesCount = c.Battles,
+                WinsCount = c.Wins
+            });
+
+            var embed = new EmbedProperties
+            {
+                Author = new EmbedAuthorProperties { Name = "Arona's intelligence report", IconUrl = botIconUrl },
+                Title = $"{name} ({ClanUtils.GetHumanRegion(region)})",
+            };
+
+            foreach (var season in processed)
+                embed.AddFields(new EmbedFieldProperties
+                {
+                    Name = $"S{season.Id} {season.Name}",
+                    Value = $"`{season.BattlesCount}` BTL -> `{Math.Round((double)season.WinsCount / season.BattlesCount * 100, 2)}%` W/B\n"
+                });
+
+            await deferredMessage.EditAsync(embed);
         }
         catch (Exception ex)
         {
